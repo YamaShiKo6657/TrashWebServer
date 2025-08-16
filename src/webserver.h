4897db409bbb65ca963c781fba0511a94d0cb672
linus <2241029754liu@gmail.com>
2025-08-16 16:49:37 +0800

    add buffer.h

---
+++ src/webserver.h
@@ -0,0 +1,15 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
+
+#define WEBSERVE_H
+const int MAX_FD=65536;
+const int MAX_EVENT_NUMBER=10000;
+const int TIMESLOT=5;
